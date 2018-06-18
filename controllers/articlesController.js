6bdb6e6d02e5ae2a97944863fd762dc937fd0cd5
TyPhyter <tylerjenkins08@gmail.com>
2018-06-18 17:15:29 -0500

    scrape articles on hits to the root route, redirect to articles route

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -9,7 +9,36 @@ const findOrCreate = require('mongoose-find-or-create');
 const Article = require('../models/Article');
 
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    request("https://techcrunch.com/", function(error, response, html) {
+        // console.log(html);
+        const $ = cheerio.load(html);
+        const results = [];
+        const articles = $('.post-block');
+        // console.log(articles);
+        articles.each(function (i, element) {
+            // console.log(element);
+            let article = {};
+            article.title = $(this).find('h2.post-block__title').text().trim();
+            article.summary = $(this).find('div.post-block__content').text().trim();
+            article.author = $(this).find('span.river-byline__authors').text().trim();
+            article.url = $(this).find('a.post-block__title__link').attr('href').trim();
+            //if the image exists
+            if($(this).find('footer img').attr('src')){
+                article.imgUrl = $(this).find('footer img').attr('src').split(/w=1[0-9][0-9][0-9][?]/).join('');
+            }
+            // console.log(article);
+            results.push(article);
+            //create new article ONLY IF no article with the same title exists
+            Article.findOrCreate({ title: article.title }, article, (err, doc) => {
+                if(err) console.log(err);
+                console.log(doc);
+            });
+                
+        });
+        
+        res.redirect('/articles');
+
+    });
 });
 
 router.get('/articles/:id?', (req, res) => {
